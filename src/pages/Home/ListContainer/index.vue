d3cfb9bc6d98410d20023377d8e1fd0d538ec9bb
gilbert777 <xjt674471772@mail.com>
2022-12-06 16:06:52 +1100

    通过this.$nextTick()和watch实现轮播图

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -5,7 +5,7 @@
         <!--banner轮播-->
         <div class="swiper-container" ref="mySwiper">
           <div class="swiper-wrapper">
-            <div class="swiper-slide" v-for="(item,index) in bannerList" :key=item.id>
+            <div class="swiper-slide" v-for="(item,index) in bannerList" :key="item.id">
               <img :src="item.imgUrl" />
             </div>
           </div>
@@ -106,34 +106,40 @@ import { mapState } from "vuex";
 export default {
   name: "",
   mounted() {
-    this.$store.dispatch("getBannerList"),
-      setTimeout(() => {
-        var mySwiper = new Swiper(this.$refs.mySwiper, {
-          //direction: "vertical", // 垂直切换选项
-          loop: true, // 循环模式选项
-
-          // 如果需要分页器
-          pagination: {
-            el: ".swiper-pagination"
-          },
-
-          // 如果需要前进后退按钮
-          navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-          },
-
-          // 如果需要滚动条
-          scrollbar: {
-            el: ".swiper-scrollbar"
-          }
-        });
-      }, 2000);
+    this.$store.dispatch("getBannerList"), setTimeout(() => {}, 0);
   },
   computed: {
     ...mapState({
       bannerList: state => state.home.bannerList
     })
+  },
+  watch: {
+    bannerList: {
+      handler(newValue, oldValue) {
+        this.$nextTick(() => {
+          var mySwiper = new Swiper(this.$refs.mySwiper, {
+            //direction: "vertical", // 垂直切换选项
+            loop: true, // 循环模式选项
+
+            // 如果需要分页器
+            pagination: {
+              el: ".swiper-pagination"
+            },
+
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev"
+            },
+
+            // 如果需要滚动条
+            scrollbar: {
+              el: ".swiper-scrollbar"
+            }
+          });
+        });
+      }
+    }
   }
 };
 </script>
